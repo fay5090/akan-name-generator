cba49b75cbadbc70ad93b053e2407e4c519c433e
fay5090 <faheemahmohamed6@gmail.com>
2026-02-03 13:09:15 +0300

    completed changes

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const maleNames = [
   "Kwaku", "Yaw", "Kofi", "Kwame"
 ];
 
-const femaleNames = [
+const femaleNames=[
   "Akosua", "Adwoa", "Abenaa",
   "Akua", "Yaa", "Afua", "Ama"
 ];
 
 
-/listen-to-form-submittion 
+//listen-to-form-submittion 
 document.getElementById("akanForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
   // Get user input
   const dob = document.getElementById("dob").value;
-  const gender = document.querySelector('input[name="gender"]:checked');checked');
+  const gender = document.querySelector('input[name="gender"]:checked'); 
 
   // Validate input
   if (!dob || !gender) {
